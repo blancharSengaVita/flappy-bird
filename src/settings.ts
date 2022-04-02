0bb4cf8580d127afccfed8666344f9bc65b146ce
Blanchar Senga-Vita <blanchar@MacBook-Air-de-Blanchar.local>
2022-04-02 11:45:10 +0200

    :wrench: added settings
    added settingns for day and night added day and night

---
+++ src/settings.ts
@@ -0,0 +1,26 @@
+export const settings = {
+    sprite : {
+        sky : {
+            day: {
+                sx:0,
+                sy:0,
+                sw:290,
+                sh: 511,
+                dx:0,
+                dy:0,
+                dw:290,
+                dh:511,
+            },
+            night : {
+                sx:290,
+                sy:0,
+                sw:290,
+                sh: 511,
+                dx:0,
+                dy:0,
+                dw:290,
+                dh:511,
+            }
+        }
+    }
+}
