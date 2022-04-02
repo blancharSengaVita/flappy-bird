7286b7de81764bb9f805dafb572a77d85c075ace
Blanchar Senga-Vita <blanchar@MacBook-Air-de-Blanchar.local>
2022-04-02 10:16:13 +0200

    :bento: updated Main.ts
    created main class  and refactor main.ts to Main.ts

---
+++ src/Main.ts
@@ -0,0 +1,10 @@
+class Main {
+    canvasElement: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+    constructor() {
+        this.canvasElement = document.getElementById("my-canvas") as HTMLCanvasElement;
+        this.ctx = this.canvasElement.getContext("2d") as CanvasRenderingContext2D;
+    }
+}
+
+new Main();
